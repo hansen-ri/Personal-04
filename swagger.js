2041a9a235992277fc8a7cf4f0d298603a4af4f9
hansen-ri <gridironhero@gmail.com>
2022-06-17 23:50:06 -0600

    URL update

--- swagger.js
+++ swagger.js
@@ -5,10 +5,10 @@ const doc = {
     title: 'Riley Hansen Contacts API',
     description: 'Contacts API'
   },
-  host: 'https://cse341-personal04.herokuapp.com/',
+  host: 'https://cse341-personal04.herokuapp.com',
   schemes: ['https']
 };
-// change host to "https://cse341-personal04.herokuapp.com/"
+// change host to "https://cse341-personal04.herokuapp.com"
 
 const outputFile = './swagger.json';
 const endpointsFiles = ['./routes/index.js'];

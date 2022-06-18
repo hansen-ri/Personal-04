8a466e4539ce524045da081c1aac58c005aaf3b4
hansen-ri <gridironhero@gmail.com>
2022-06-17 23:27:03 -0600

    heroku update

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,18 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/contacts', require('./contacts'))
+router.use('/', require('./swagger'));
 
-router.get('/contacts/:id', require('./contacts'));
+// router.get('/contacts', require('./contacts'))
 
-router.post('/contacts', require('./contacts'));
+// router.get('/contacts/:id', require('./contacts'));
 
-router.put('/contacts/:id', require('./contacts'));
+// router.post('/contacts', require('./contacts'));
 
-router.delete('/contacts/:id', require('./contacts'));
+// router.put('/contacts/:id', require('./contacts'));
 
-// router.use('/Contacts', require('./contacts'));
+// router.delete('/contacts/:id', require('./contacts'));
+
+router.use('/', require('./contacts'));
 
 module.exports = router
---
+++ swagger.js
@@ -0,0 +1,22 @@
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc = {
+  info: {
+    title: 'Riley Hansen Contacts API',
+    description: 'Contacts API'
+  },
+  host: 'https://cse341-personal04.herokuapp.com/',
+  schemes: ['https']
+};
+// change host to "https://cse341-personal04.herokuapp.com/"
+
+const outputFile = './swagger.json';
+const endpointsFiles = ['./routes/index.js'];
+
+// generate swagger.json
+swaggerAutogen(outputFile, endpointsFiles, doc);
+
+// Run server after it gets generated
+// swaggerAutogen(outputFile, endpointsFiles, doc).then(async () => {
+//   await import('./index.js');
+// });
